a9178dd90158e2959a86760bbd848a936f947c61
github username <github email address>
2021-06-16 17:23:57 -0600

    removed default render

--- src/App.js
+++ src/App.js
@@ -3,24 +3,13 @@ import './App.css';
 import React from 'react';
 
 class App extends React.Component {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+  render(){
+    return (
+      "hello I'm React"
+    );
+  }
+
 }
 
 export default App;
