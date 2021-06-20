310efe081bc1d5d19809da287164ec2071dcb643
github username <github email address>
2021-06-19 21:52:53 -0600

    fixed typos

--- src/actions/currentUser.js
+++ src/actions/currentUser.js
@@ -35,20 +35,11 @@ export const login = credentials => {
 
 export const logout = () => {
     return dispatch => {
-        dispatch(clearCurrentUser)
+        dispatch(clearCurrentUser())
         return fetch("http://localhost:3000/api/v1/logout", {
             credentials: "include",
             method: "DELETE"
         })
-        .then(r => r.json())
-        .then(user => {
-            if (user.error) {
-                alert(user.error)
-            } else {
-                dispatch(setCurrentUser(user))
-            }
-        })
-        .catch()
     }
 }
 
