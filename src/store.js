995ecc513d9cabba5fd947b47e45a1a17d4f0849
github username <github email address>
2021-06-25 14:00:59 -0600

    added recipeForm reducer

--- src/store.js
+++ src/store.js
@@ -1,10 +1,11 @@
-import { createStore, applyMiddleware, compose, combineReducers } from 'redux'
-import thunk from 'redux-thunk'
+import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
+import thunk from 'redux-thunk';
 import currentUser from './reducers/currentUser.js';
-import users from './reducers/users.js'
-import loginForm from './reducers/loginForm.js'
-import recipes from './reducers/recipes.js'
-import signupForm from './reducers/signupForm'
+import users from './reducers/users.js';
+import loginForm from './reducers/loginForm.js';
+import recipes from './reducers/recipes.js';
+import signupForm from './reducers/signupForm.js';
+import recipeForm from './reducers/recipeForm.js'
 
 
 const reducer = combineReducers({
@@ -12,7 +13,8 @@ const reducer = combineReducers({
   currentUser,
   loginForm,
   recipes,
-  signupForm
+  signupForm,
+  recipeForm
 })
 
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
