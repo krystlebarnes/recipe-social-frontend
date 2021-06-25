f399ec94f826df8c89c50783e648660ba8b01a87
github username <github email address>
2021-06-25 14:04:52 -0600

    added NewRecipeFormWrapper component to handle recipe form submission for new recipes

---
+++ src/components/NewRecipeFormWrapper.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import RecipeForm from './RecipeForm'
+import { addRecipe } from '../actions/recipes.js'
+import { connect } from 'react-redux'
+
+const NewRecipeFormWrapper = ({ history, addRecipe }) => {
+
+  const handleSubmit = (formData, authorId) => {
+    addRecipe({
+      ...formData,
+      authorId
+    }, history)
+  }
+  return  <RecipeForm history={history} handleSubmit={handleSubmit} />
+};
+
+export default connect(null, { addRecipe })(NewRecipeFormWrapper);
