54488dfb65d63f3ac10dd8a057e50de78560d1a7
github username <github email address>
2021-06-25 20:29:43 -0600

    added reducers for ADD_RECIPE, EDIT_RECIPE, and DELETE_RECIPE

--- src/reducers/recipes.js
+++ src/reducers/recipes.js
@@ -2,6 +2,12 @@ export default (state = [], action) => {
     switch (action.type) {
         case "SET_RECIPES":
             return action.recipes
+        case "ADD_RECIPE":
+            return state.concat(action.trip)
+        case "EDIT_RECIPE":
+            return state.map(recipe => recipe.id === action.recipe.id ? action.recipe : recipe)
+        case "DELETE_RECIPE":
+            return state.filter(recipe => recipe.id === action.recipeId ? false : true)
         default:
             return state
     }
