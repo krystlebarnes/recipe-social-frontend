7f37d8247e950d44e0793ee133ced39d3bebea3a
github username <github email address>
2021-06-25 20:31:12 -0600

    added addRecipe action

---
+++ src/actions/addRecipe.js
@@ -0,0 +1,34 @@
+export const addRecipe = (recipeData, history) => {
+    return dispatch => {
+        const newRecipe = {
+            name: recipeData.name,
+            image_url: recipeData.imageURL,
+            description: recipeData.description,
+            prep_time: recipedData.prepTime,
+            cook_time: recipeData.cookTime,
+            serving_size: recipeData.servingSize,
+            calories: recipeData.calories,
+            author_id: recipeData.authorId
+        }
+        return fetch("http://localhost:3000/api/v1/recipes", {
+            credentials: "include",
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(newRecipe)
+        })
+        .then(r => r.json())
+        .then(r => {
+            if (r.error) {
+                alert(r.error)
+            } else {
+                const recipe = r.data
+                dispatch({type: 'ADD_RECIPE', recipe})
+                dispatch(resetRecipeForm())
+                history.push(`/recipes/${r.data.id}`)
+            }
+        })
+        .catch(console.log)
+    }
+}
