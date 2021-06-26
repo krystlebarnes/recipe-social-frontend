e241b6189a0dc7fbf5723eb2728c1e4e2306b378
github username <github email address>
2021-06-25 20:32:48 -0600

    added editRecipe action

---
+++ src/actions/editRecipe.js
@@ -0,0 +1,32 @@
+export const editRecipe = (recipeData, history) => {
+    return dispatch => {
+        const updatedRecipe = {
+            name: recipeData.name,
+            image_url: recipeData.imageURL,
+            description: recipeData.description,
+            prep_time: recipedData.prepTime,
+            cook_time: recipeData.cookTime,
+            serving_size: recipeData.servingSize,
+            calories: recipeData.calories
+        }
+        return fetch("http://localhost:3000/api/v1/recipes/${recipeData.recipeId}", {
+            credentials: "include",
+            method: "PATCH",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(updatedRecipe)
+        })
+        .then(r => r.json())
+        .then(r => {
+            if (r.error) {
+                alert(r.error)
+            } else {
+                const recipe = r.data
+                dispatch({type: 'EDIT_RECIPE', recipe})
+                history.push(`/recipes/${r.data.id}`)
+            }
+        })
+        .catch(console.log)
+    }
+}
