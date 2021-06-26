569ae15c1abf8c300cee34b9c2dc9d9a8693b1b6
github username <github email address>
2021-06-25 20:18:33 -0600

    add setFormData action

--- src/actions/recipeForm.js
+++ src/actions/recipeForm.js
@@ -10,3 +10,19 @@ export const resetRecipeForm = () => {
         type: "RESET_RECIPE_FORM"
     }
 }
+
+export const setFormData = recipe => {
+    const recipeFormData = {
+      name: recipe.attributes.name,
+      imageURL: recipe.attributes.image_url,
+      description: recipe.attributes.description,
+      prepTime: recipe.attributes.prep_time,
+      cookTime: recipe.attributes.cook_time,
+      servingSize: recipe.attributes.serving_size,
+      calories: recipe.attributes.calories
+    }
+    return {
+      type: "SET_FORM_DATA",
+      recipeFormData
+    }
+  }
