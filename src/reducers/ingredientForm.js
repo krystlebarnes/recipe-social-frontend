8b1e82a2eb9e45f1954a52b3b35cead8d58d6737
github username <github email address>
2021-06-25 22:25:48 -0600

    add update and reset ingredient reducers

---
+++ src/reducers/ingredientForm.js
@@ -0,0 +1,18 @@
+const initialState = {
+    quantity: "",
+    unit: "",
+    item: "",
+    notes: "",
+    recipeId: ""
+}
+  
+export default (state=initialState, action) => {
+    switch (action.type) {
+        case "UPDATE_INGREDIENT_FORM":
+            return action.formData
+        case "RESET_INGREDIENT_FORM":
+            return initialState
+        default:
+            return state
+    }
+  }
