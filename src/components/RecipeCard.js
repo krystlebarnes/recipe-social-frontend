116ecfd89d5eb3481db5fd5ac60b55c970e8c9c7
github username <github email address>
2021-06-26 09:13:33 -0600

    imported Link and fixed className typo

--- src/components/RecipeCard.js
+++ src/components/RecipeCard.js
@@ -1,13 +1,16 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const RecipeCard = ({ recipe }) => {
     return (
-        <div class="RecipeCard">
+        <Link to={`/recipes/${recipe.id}`}>
+        <div className="RecipeCard">
             <img src={ recipe.attributes.image_url }></img>
             <h3>{ recipe.attributes.name }</h3>
             <span>{ recipe.attributes.description }</span>
             {/* Add ratings, likes, comments icon to expand comments, and link to view full recipe details */}
         </div>
+        </Link>
     )
 }
 
