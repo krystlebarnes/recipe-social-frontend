becbce452f4858fefbfed53fbdf1284c178b1a0d
github username <github email address>
2021-07-04 23:52:41 -0600

    removed unused reducer

--- src/reducers/recipeForm.js
+++ src/reducers/recipeForm.js
@@ -11,8 +11,6 @@ const initialState = {
   
 export default (state=initialState, action) => {
     switch (action.type) {
-        case "UPDATE_RECIPE_FORM":
-            return action.formData
         case "RESET_RECIPE_FORM":
             return initialState
         default:
